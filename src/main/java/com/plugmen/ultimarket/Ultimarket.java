6c8772c4197d862e715f7dd98ef12e634f2c5c34
Andrew Stein <stein.andrew.01@gmail.com>
2022-10-15 17:20:21 -0400

    Initial commit

---
+++ src/main/java/com/plugmen/ultimarket/Ultimarket.java
@@ -0,0 +1,17 @@
+package com.plugmen.ultimarket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ultimarket extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
